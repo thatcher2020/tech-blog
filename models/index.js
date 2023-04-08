cb2bf2555852c1f3c3ea218baf9944ebfa5e1315
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-04-08 19:43:47 -0400

    models done and added comments.js

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
