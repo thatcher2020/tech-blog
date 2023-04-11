6d05d0aa8c4c2f17190f82b59e2fafb2166e263d
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-04-11 15:18:03 -0400

    login and logout done

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const loginFromHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (email && username && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ email, username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFromHandler);
+
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
