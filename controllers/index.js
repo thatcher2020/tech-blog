5473e9724fe05d69dc151f5fbee3219887de3992
Joseph Thatcher <thatcherjoe20@gmail.com>
2023-04-13 17:19:26 -0400

    fixed some routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./api/comment-routes');
 
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
